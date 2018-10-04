79c663522619fa1169cd74dc93f00b3c2c7552e3
Leo Lee <yichenglee_leo@outlook.com>
2018-10-05 07:13:03 +0800

    Add new file

---
+++ if1.py
@@ -0,0 +1,12 @@
+age = input('請輸入年齡: ')
+
+#casting 型別轉換
+age = int(age)
+if age >= 20:
+   print('你可以投票')
+
+
+celsius = input('請輸入攝氏溫度: ')
+fahrenheit = float(celsius) * 9 / 5 + 32
+print('華氏溫度為: ', fahrenheit)
+
